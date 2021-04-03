63c90c00f8f3bbcd08ce6d8de6a254b5bf0e0665
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-04-03 12:14:09 -0500

    add worked and neested operation.

--- index.js
+++ index.js
@@ -1,54 +1,99 @@
 class Calculator{
-  constructor(){
-    
+  sum(a, b){
+    return a + b
   }
-  sum(a , b){
-
-  }
-  subtraction(a ,b){
-
+  subtraction(a, b){
+    return a - b;
   }
   division(d, n){
-    
+    return d/n
   }
   multiplication(a, b){
-
+    return a * b
   }
-  module(num , n){
-
+  module(num, n){
+    return num%n
   }
-}
+};
 
 
 document.addEventListener('DOMContentLoaded', ()=> {
-  const calculator = new Calculator();
+  let x = 0;
+  let y = 0;
+  let o = 0;
 
-const display = document.querySelector('.calculator__display');
-const number = document.querySelectorAll('.number');
-const ac = document.getElementById('ac');
-const c = document.getElementById('c');
-c.addEventListener('click', (ev) => {
-  display.value = ''
-})
-ac.addEventListener('click', (ev) => {
-   let str = display.value;
-   str = str.split('');
-   str.pop()
-   display.value = str.join('')
-   console.log(str)
-})
-number.forEach( button => {
-  button.addEventListener("click", (ev)=>{
-    setValueInDisplay(ev.target.value)
-  })
+  const display = document.querySelector('.calculator__display');
+  const numbers = document.querySelectorAll('.number');
+  const ac = document.getElementById('ac');
+  const c = document.getElementById('c');
+  const operations = document.querySelectorAll('.operation');
+  const submit = document.getElementById('submit');
+
+  c.addEventListener('click', restart);
+
+  ac.addEventListener('click', ( ev ) => {
+    let str = display.value;
+    str = str.split('');
+    str.pop()
+    display.value = str.join('')
+  });
+  submit.addEventListener('click', ( ev ) => {
+    let calculator = new Calculator();
+    saveInputs(display.value)
+    switch (o) {
+      case "+":
+        display.value = calculator.sum(x,y);
+        break;
+      case "-":
+        display.value = calculator.subtraction(x,y);
+        break;
+      case "*":
+        display.value = calculator.multiplication(x,y);
+        break;
+      case "/":
+        display.value = calculator.division(x,y);
+        break;
+      case "%":
+        display.value = calculator.module(x,y);
+        break;
+    }
+      x = new Number(display.value);
+      y = 0;
+    console.log(x,y)
+  });
+  operations.forEach( operation => {
+    operation.addEventListener('click', ( ev ) => {
+      o = operation.value;
+      saveInputs(display.value)
+      display.value = ''
+    });
+  });
+  numbers.forEach( number => {
+    number.addEventListener('click', ( ev )=>{
+      setValueInDisplay(ev.target.value)
+    });
+  });
+
+  function setValueInDisplay( value ){
+    display.value += value
+  };
+  function saveInputs(value) {
+    if( x == 0){
+      x = new Number(value);
+    }else{
+      y = new Number(value);
+    }
+    console.log(x,y)
+  };
+  function restart() {
+    display.value = ''
+    x = 0;
+    y = 0;
+    o = 0;  
+    emptyValues = false;
+  };
 });
-function setValueInDisplay( value ){
-  display.value += value
-}
-
-})
-
-
+//x, y, o
 
 
 
